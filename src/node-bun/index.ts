d244acf4ba9b4635d3944909c2173c144c1ca422
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2022-12-13 15:23:36 +0700

    fix: separate in-memory operation to it's own file

--- src/node-bun/index.ts
+++ src/node-bun/index.ts
@@ -1,30 +1,35 @@
-import type { PersistenceFormat } from '../common/types'
-import { readFileSync, writeFileSync } from 'fs'
-import { join } from 'path'
-import { Lyra, PropertiesSchema } from '@lyrasearch/lyra'
-import { getDefaultFileName, persist as persistDB, restore as restoreDB } from '../common/utils'
+import type { PersistenceFormat } from "../common/types";
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+import { Lyra, PropertiesSchema } from "@lyrasearch/lyra";
+import {
+  getDefaultFileName,
+  persist as persistDB,
+  restore as restoreDB,
+} from "../common/utils";
 
-function getDefaultOutputDir (format: PersistenceFormat): string {
-  return join(process.cwd(), getDefaultFileName(format, 'node'))
+function getDefaultOutputDir(format: PersistenceFormat): string {
+  return join(process.cwd(), getDefaultFileName(format, "node"));
 }
 
-export function persistToFile<T extends PropertiesSchema> (db: Lyra<T>, format: PersistenceFormat = 'binary', path: string = getDefaultOutputDir(format)): string {
-  const serialized = persistDB(db, format)
+export function persistToFile<T extends PropertiesSchema>(
+  db: Lyra<T>,
+  format: PersistenceFormat = "binary",
+  path: string = getDefaultOutputDir(format)
+): string {
+  const serialized = persistDB(db, format);
 
-  writeFileSync(path, serialized)
+  writeFileSync(path, serialized);
 
-  return path
+  return path;
 }
 
-export function restoreFromFile<T extends PropertiesSchema> (format: PersistenceFormat = 'binary', path: string = getDefaultOutputDir(format)): Lyra<T> {
-  const data = readFileSync(path)
-  return restoreDB(format, data)
+export function restoreFromFile<T extends PropertiesSchema>(
+  format: PersistenceFormat = "binary",
+  path: string = getDefaultOutputDir(format)
+): Lyra<T> {
+  const data = readFileSync(path);
+  return restoreDB(format, data);
 }
 
-export function importInstance<T extends PropertiesSchema>(data: string | Buffer, format: PersistenceFormat): Lyra<T> {
-  return restoreDB(format, data)
-}
-
-export function exportInstance<T extends PropertiesSchema> (db: Lyra<T>, format: PersistenceFormat = 'binary'): string | Buffer {
-  return persistDB(db, format)
-}
+export { exportInstance, importInstance } from "./memory";
